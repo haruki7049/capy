83f9fc935456e73f2df30e8b6712c08844539d5a
haruki7049 <tontonkirikiri@gmail.com>
2025-07-01 20:48:38 +0900

    refactor(flake.nix): Update syntax

--- flake.nix
+++ flake.nix
@@ -18,69 +18,49 @@
           inherit system;
           overlays = [ zig-overlay.overlays.default ];
         };
-
-        # The project requires exactly this Zig version (2024.11.0-mach)
-        zigPkg = pkgs.stdenv.mkDerivation rec {
-          pname = "zig";
-          version = "0.14.1";
-          
-          src = pkgs.fetchurl {
-            url = "https://ziglang.org/download/${version}/zig-x86_64-linux-${version}.tar.xz";
-            sha256 = "sha256-e+ar3r+pcMYTjRZbNI0EZOhPFvUx5xyyDA4FL64djI0=";
-          };
-          
-          installPhase = ''
-            mkdir -p $out/bin
-            cp zig $out/bin/
-            chmod +x $out/bin/zig
-            
-            mkdir -p $out/lib
-            cp -r lib/* $out/lib/
-          '';
-          
-          dontFixup = true;
-        };
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
+        devShells.default = pkgs.mkShell rec {
+          nativeBuildInputs = [
             # Core development tools
-            zigPkg
-            
+            pkgs.zigpkgs."0.14.1"
+
             # Build tools
-            gnumake
-            pkg-config
-            
-            # GTK and related libraries for Linux backend
-            gtk3
-            gtk4
-            glib
-            cairo
-            pango
-            gdk-pixbuf
+            pkgs.pkg-config
+            pkgs.gnumake
             
             # Android development (optional)
-            android-tools
+            pkgs.android-tools
+
+            # Development utilities
+            pkgs.gdb
+            pkgs.valgrind
+            pkgs.strace
+          ];
+
+          buildInputs = [
+            # GTK and related libraries for Linux backend
+            pkgs.gtk3
+            pkgs.gtk4
+            pkgs.glib
+            pkgs.cairo
+            pkgs.pango
+            pkgs.gdk-pixbuf
             
             # OpenGL/Graphics
-            libGL
-            libGLU
-            mesa
+            pkgs.libGL
+            pkgs.libGLU
+            pkgs.mesa
             
             # Audio libraries
-            alsa-lib
-            pipewire
-            
-            # Development utilities
-            gdb
-            valgrind
-            strace
+            pkgs.alsa-lib.dev
+            pkgs.pipewire.dev
             
             # Code formatting and linting
-            zls # Zig Language Server
+            pkgs.zls # Zig Language Server
             
             # Version control
-            git
+            pkgs.git
           ];
 
           shellHook = ''
@@ -119,19 +99,11 @@
             echo "  zig build transition"
             echo "  zig build weather"
             echo ""
-            
-            # Set up pkg-config paths for GTK
-            export PKG_CONFIG_PATH="${pkgs.gtk3}/lib/pkgconfig:${pkgs.gtk4}/lib/pkgconfig:$PKG_CONFIG_PATH"
-            
-            # Set up library paths
-            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
-              pkgs.gtk3
-              pkgs.gtk4
-              pkgs.libGL
-              pkgs.mesa
-              pkgs.alsa-lib
-            ]}:$LD_LIBRARY_PATH"
           '';
+
+          env = {
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          };
         };
       });
 }
